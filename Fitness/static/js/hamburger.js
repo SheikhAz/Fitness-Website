523723d9e93a0399a51247b1ad45aa59a4ecf6b3
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-02-27 17:30:36 +0530

    Frontent Design is Completed

--- Fitness/static/js/hamburger.js
+++ Fitness/static/js/hamburger.js
@@ -4,38 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeBtn = document.getElementById("closeMenu");
   const links = popupMenu.querySelectorAll("a");
 
+  // =====================
+  // OPEN MENU
+  // =====================
   function openMenu() {
-    popupMenu.classList.remove(
-      "opacity-0",
-      "-translate-y-20",
-      "pointer-events-none",
-    );
-
-    popupMenu.classList.add(
-      "opacity-100",
-      "translate-y-24",
-      "pointer-events-auto",
-    );
+    popupMenu.classList.remove("opacity-0", "pointer-events-none");
+    popupMenu.classList.add("opacity-100", "pointer-events-auto");
   }
 
+  // =====================
+  // CLOSE MENU
+  // =====================
   function closeMenu() {
-    popupMenu.classList.add(
-      "opacity-0",
-      "-translate-y-20",
-      "pointer-events-none",
-    );
-
-    popupMenu.classList.remove(
-      "opacity-100",
-      "translate-y-24",
-      "pointer-events-auto",
-    );
+    popupMenu.classList.add("opacity-0", "pointer-events-none");
+    popupMenu.classList.remove("opacity-100", "pointer-events-auto");
   }
 
   menuBtn.addEventListener("click", openMenu);
   closeBtn.addEventListener("click", closeMenu);
 
-  // close when link clicked
   links.forEach((link) => {
     link.addEventListener("click", closeMenu);
   });
