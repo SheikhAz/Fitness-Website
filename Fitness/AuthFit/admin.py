cb51a206a1bf8747cb5fb6732e27e0eb6c60b400
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-03-01 02:05:05 +0530

    Enrollment Model is created.

--- Fitness/AuthFit/admin.py
+++ Fitness/AuthFit/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from AuthFit.models import Contact
+from AuthFit.models import Contact , Enrollment
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(Enrollment)
--- Fitness/AuthFit/models.py
+++ Fitness/AuthFit/models.py
@@ -9,3 +9,32 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Enrollment(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    MembershipPlan = [
+        ('B' ,'Basic'),
+        ('P' ,'Popular'),
+        ('A' ,'Annual'),
+    ]
+    Trainer = [
+        ('Y','Yes'),
+        ('N','No')
+    ]
+    fullname = models.CharField(max_length=25)
+    email = models.EmailField(null=True ,blank=True)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
+    phonenumber = models.CharField(max_length=10)
+    dob = models.DateField()
+    doj = models.DateTimeField()
+    plan = models.CharField(max_length=1,choices=MembershipPlan,default='B')
+    trainer = models.CharField(max_length=1,choices=Trainer ,default='N')
+    Reference = models.CharField(max_length=30,null=True,blank=True)
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.fullname}"
