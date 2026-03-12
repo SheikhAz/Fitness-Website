d3fd7414b16bdede532c3ffae2b51c999afc2bb4
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-03-12 13:30:26 +0530

    fix node path

--- Fitness/settings.py
+++ Fitness/settings.py
@@ -133,4 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TAILWIND_APP_NAME = 'theme'
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+if os.name == "nt":  # Windows
+    NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+else:  # Linux (Render)
+    NPM_BIN_PATH = "/usr/bin/npm"
