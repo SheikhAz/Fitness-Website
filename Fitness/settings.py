6f573a5319b88f6f3f19e69e0aa79e5cb8afcd9c
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-03-12 17:09:31 +0530

    Updated

--- Fitness/settings.py
+++ Fitness/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'AuthFit',
     'tailwind',
     'theme',
+    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
