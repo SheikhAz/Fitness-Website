a9b773694979e77cfa0353da0434f2655a3aa871
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-03-26 19:33:50 +0530

    Final changes

--- staticfiles_build/js/RefreshTrigger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hero = document.getElementById("heroContent");
-
-  setTimeout(() => {
-    hero.classList.remove("opacity-0");
-    hero.classList.add("hero-animate");
-  }, 200); // small delay for smooth start
-});
